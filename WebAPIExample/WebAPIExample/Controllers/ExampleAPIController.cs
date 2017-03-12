1a2e79ebfb256919959d0b6379aaa4d2146edf16
Md. Almajid KOUSHiK <Md. Almajid KOUSHiK>
2017-03-13 00:32:47 +0600

    Ini Commit.....

---
+++ WebAPIExample/WebAPIExample/Controllers/ExampleAPIController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebAPIExample.Controllers
+{
+    public class ExampleAPIController : ApiController
+    {
+        public async Task<string> Post()
+        {
+            string returnValue = "OK";
+
+            var formKeyValueData = await Request.Content.ReadAsFormDataAsync();
+
+            string AuthorizedKey = formKeyValueData["AuthorizedKey"];
+
+            return returnValue;
+        }
+
+        public string Get(string AuthorizedKey)
+        {
+            string returnValue = "OK";
+            return returnValue;
+        }
+    }
+}
